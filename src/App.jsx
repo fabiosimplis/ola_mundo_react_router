dad1c66835377354d3be3559cbc80305ad710a37
fabiosimplis <fabio.simplis@gmail.com>
2025-01-25 09:50:18 -0300

    add `react-router` e criando o roteador

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./assets/components/Inicio";
 import SobreMim from "./assets/components/SobreMim";
 
-console.log(window.location);
-
-const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
-
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
