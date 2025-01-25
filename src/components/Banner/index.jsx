bd73abdb706c687dcb64703b23152b3f529d7fbf
fabiosimplis <fabio.simplis@gmail.com>
2025-01-25 13:54:15 -0300

    add Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,26 @@
+import styles from "./Banner.module.css";
+import circuloColorido from "../../assets/circulo_colorido.png";
+export function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titu}>Olá, Mundo!</h1>
+
+        <p className={styles.paragrafo}>
+          Boas vindas ao meu espaço. Eu sou Fábio, desenvolvedor full stack.
+          Aqui vou compartilhar meu projetos.
+        </p>
+      </div>
+      <div className={styles.imagens}>
+        <img
+          className={styles.circuloColorido}
+          src={circuloColorido}
+          aria-hidden={true}
+        />
+        <img 
+          className={styles.minhaFoto}
+          src="https://github.com/fabiosimplis.png" alt="Foto de Fábio" />
+      </div>
+    </div>
+  );
+}
--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -4,7 +4,7 @@ import styles from "./MenuLink.module.css";
 export default function MenuLink({ children, to }) {
   return (
     <NavLink
-      className={({ isActive }) => `${styles.link} ${isActive ? styles.linkDestacado : ""}`}
+      className={({ isActive }) => `${styles.link} ${ isActive ? styles.linkDestacado : ""}`}
       to={to}
       end
     >
