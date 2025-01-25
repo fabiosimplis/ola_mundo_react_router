807966c3cb34e22fcc34db22df83f03c5ddc6e6c
fabiosimplis <fabio.simplis@gmail.com>
2025-01-25 11:53:46 -0300

    add `useLocation`

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import Inicio from "./assets/components/Inicio";
 import SobreMim from "./assets/components/SobreMim";
 import Menu from "./assets/components/Menu";
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
@@ -16,4 +16,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppRoutes;
