8048f0178e9177a7a5c1f6877e6e25d695e6a074
fabiosimplis <fabio.simplis@gmail.com>
2025-01-25 10:43:06 -0300

    add `Link` do `react-router-dom`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./assets/components/Inicio";
 import SobreMim from "./assets/components/SobreMim";
+import Menu from "./assets/components/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
