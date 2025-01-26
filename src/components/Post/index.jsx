e93b9341dde483fc72164a75807260c99a490815
fabiosimplis <fabio.simplis@gmail.com>
2025-01-26 14:40:27 -0300

    add solução alternativa com `children`

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -16,22 +16,15 @@ export default function Post() {
     return <NaoEncontrada />;
   }
   return (
-    <Routes>
-      <Route path="*" element={<PaginaPadrao />}>
-        <Route
-          index
-          element={
-            <PostModelo
-              fotoCapa={`/assets/posts/${post.id}/capa.png`}
-              titulo={post.titulo}
-            >
-              <div className="post-markdown-container">
-                <ReactMarkdown>{post.texto}</ReactMarkdown>
-              </div>
-            </PostModelo>
-          }
-        />
-      </Route>
-    </Routes>
+    <PaginaPadrao>
+      <PostModelo
+        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+        titulo={post.titulo}
+      >
+        <div className="post-markdown-container">
+          <ReactMarkdown>{post.texto}</ReactMarkdown>
+        </div>
+      </PostModelo>
+    </PaginaPadrao>
   );
 }
